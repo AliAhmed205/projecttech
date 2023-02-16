16269c14f8a43a6199c51381cc4f784d073033e8
AliAhmed205 <ali_lazuli@outlook.com>
2023-02-16 13:52:24 +0100

    init commit

---
+++ student.js
@@ -0,0 +1,15 @@
+
+function sum() {
+  var args = arguments
+  var total = 0
+  var index = -1
+
+
+  while (++index < args.length) {
+    total += args[index]
+  }
+
+  return total
+}
+
+module.exports = sum
